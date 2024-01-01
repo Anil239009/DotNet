be2d4f3715cb6ee5ee6fa8291adbc744277baa7a
Anil239009 <anilbemech2020@gmail.com>
2024-01-01 22:51:00 +0530

    Employee class

---
+++ EmployeeSolutoin/FirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Maths;
+using HR;
+using GDI;
+
+Person person1 = new Person();
+person1.Name = "Anil";
+person1.Age = 25;
+person1.Id = 11;
+
+Person person2 = new Person(22,"Anuj", 22);
+Person person2 = new Person(23,"Anuj", 33);
+
+Console.WriteLine(person1);
+Console.WriteLine(person2);
+
+
+
