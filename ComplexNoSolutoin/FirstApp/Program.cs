eb435a371f7d1635acf157f34356a6ebc2bb603c
Anil239009 <anilbemech2020@gmail.com>
2024-01-01 22:47:24 +0530

    Operator overloading

---
+++ ComplexNoSolutoin/FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Maths;
+
+var c1 = new Complex(67, 34);
+var c2 = new Complex(12, 33);
+var c3 = c1 + c2;
+var c4 = c1 - c2;
+
+Console.WriteLine(c1);
+Console.WriteLine(c2);
+Console.WriteLine(c3);
+Console.WriteLine(c4);
+
+
+
